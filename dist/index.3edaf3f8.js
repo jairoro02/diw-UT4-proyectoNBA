86e26bcbeadf63a2916512c88a6b91df7bc3accd
jairoromero <jromero.alumno@iesrafaelalberti.es>
2022-12-11 23:17:56 +0100

    perfil

---
+++ dist/index.3edaf3f8.js
@@ -0,0 +1,7 @@
+const btnCambio = document.querySelector("#cambio");
+btnCambio.addEventListener("click", ()=>{
+    document.body.classList.toggle("oscuro");
+    btnCambio.classList.toggle("active");
+});
+
+//# sourceMappingURL=index.3edaf3f8.js.map
